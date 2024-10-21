e2f05b0276566ccd78401f59f83b908109c4be08
erkansevim <erkansevim.essgmail.com>
2024-10-21 10:26:56 +0200

    req + res

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
  require('dotenv').config()
  const PORT = process.env.PORT
  const HOST = process.env.HOST
- 
+
  const express=require('express')
  const app=express()
- const port=process.env.PORT
+
  app.get('/',(req,res)=>{
      res.send('hello world')
  })
- app.listen(port,()=>{
-     console.log(`server is running on port ${port}`)
+ app.listen(PORT,()=>{
+     console.log(`server is running on port ${HOST}:${PORT}`)
  })
